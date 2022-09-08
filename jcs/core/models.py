b9ced9b24e4a550af6cdd80a84f3da96d07ce8e1
martin <martinsischik@gmail.com>
2022-09-08 17:20:08 -0400

    models

--- jcs/core/models.py
+++ jcs/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Employee ( models.Model ) :
+    names = models.CharField ( max_length = 150 , verbose_name = ' Nombres ' )
+    ci = models.CharField ( max_length = 10 , unique = True , verbose_name = ' Ci ' )
+
+    def __str__(self):
+        return self.names
+
+    class Meta:
+        verbose_name = 'Empleado'
+        verbose_name_plural = 'Empleados' 
+        db_table = 'empleado'
+        ordering = ['id']
+    

1b33fd6b44ab0f4a5e03b48983f72499c5e60f34
martin <martinsischik@gmail.com>
2022-09-08 15:01:11 -0400

    se agrega la tabla empleado

--- jcs/core/app/models.py
+++ jcs/core/app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Empleado(models.Model):
+    names = models.CharField(max_length=100,verbose_name='Nombres')
+    ci = models.CharField(max_length=10,unique=True,verbose_name='CI')

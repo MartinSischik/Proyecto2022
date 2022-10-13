22e74a33d093e2be806855ce629ea8fc4534a573
martin <martinsischik@gmail.com>
2022-10-13 15:13:25 -0300

    tests

--- jcs/core/tests.py
+++ jcs/core/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from jcs.wsgi import *
+from core.models import Type
 
-# Create your tests here.
+#listar
+
+#select * from tabla
+
+# query = Type.objects.all()
+# print (query)
+
+#insertar
+
+#t = Type()
+#t.name = '24/h'
+#t.save()
+
+#edicion
+# t=Type.objects.get(id=1)
+# t.name ='muñaño'
+# t.save()
+
+#eliminacion
+
+# t=Type.objects.get(id=1)
+# t.delete()
+
+obj=Type.objects.filter(name__icontains='tiempo').count()
+print(obj)
